111fe4a90b3e7c5bde1a0955671eb49ef19e9caf
florian <florian.guiffault@softeam.fr>
2018-09-10 15:13:35 +0200

    Init

---
+++ social.php
@@ -0,0 +1,64 @@
+<?php
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+use RocketTheme\Toolbox\Event\Event;
+
+/**
+ * Class SocialPlugin
+ * @package Grav\Plugin
+ */
+class SocialPlugin extends Plugin
+{
+    /**
+     * @return array
+     *
+     * The getSubscribedEvents() gives the core a list of events
+     *     that the plugin wants to listen to. The key of each
+     *     array section is the event that the plugin listens to
+     *     and the value (in the form of an array) contains the
+     *     callable (or function) as well as the priority. The
+     *     higher the number the higher the priority.
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0]
+        ];
+    }
+
+    /**
+     * Initialize the plugin
+     */
+    public function onPluginsInitialized()
+    {
+        // Don't proceed if we are in the admin plugin
+        if ($this->isAdmin()) {
+            return;
+        }
+
+        // Enable the main event we are interested in
+        $this->enable([
+            'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
+            'onTwigSiteVariables' => ['onTwigSiteVariables', 0],
+        ]);
+    }
+
+    /**
+     * Add current directory to twig lookup paths.
+     */
+    public function onTwigTemplatePaths()
+    {
+        $this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
+    }
+
+    /**
+     * if enabled on this page, load the JS + CSS theme.
+     */
+    public function onTwigSiteVariables()
+    {
+        $twig = $this->grav['twig'];
+        $twig->twig_vars['social_buttons'] = $this->config->get('plugins.social.buttons');
+        $this->grav['assets']->add('plugin://social/css/custom.css');
+    }
+}
